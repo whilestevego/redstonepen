72ac02a69cf684fceeabae53698ec7c9bb5f2139
Steve Godin <whilestevego@users.noreply.github.com>
2026-05-12 21:20:02 -0600

    Migrate ModRenderersTest and ControlBoxTest to Kotest; delete McBootstrap (#71)

--- common/src/test/kotlin/wile/redstonepen/KotestProjectConfig.kt
+++ common/src/test/kotlin/wile/redstonepen/KotestProjectConfig.kt
@@ -1,9 +1,12 @@
 package wile.redstonepen
 
 import io.kotest.core.config.AbstractProjectConfig
+import net.minecraft.SharedConstants
+import net.minecraft.server.Bootstrap
 
 object KotestProjectConfig : AbstractProjectConfig() {
     override suspend fun beforeProject() {
-        McBootstrap.bootstrap()
+        SharedConstants.tryDetectVersion()
+        Bootstrap.bootStrap()
     }
 }
